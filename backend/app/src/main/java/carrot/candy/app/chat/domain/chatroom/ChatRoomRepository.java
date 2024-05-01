0c198fa719376c45539fbc325f792c15d4011a85
jinsu868 <mjinsu098@gmail.com>
2024-05-01 23:31:04 +0900

    fix: chatRoom 조회 dao 수정 (#7)

--- backend/app/src/main/java/carrot/candy/app/chat/domain/chatroom/ChatRoomRepository.java
+++ backend/app/src/main/java/carrot/candy/app/chat/domain/chatroom/ChatRoomRepository.java
@@ -1,6 +1,7 @@
 package carrot.candy.app.chat.domain.chatroom;
 
 import carrot.candy.app.member.domain.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     boolean existsByOwnerAndVisitor(Member owner, Member visitor);
 
-    @Query("select cr from  ChatRoom  cr join fetch cr.owner o join fetch cr.visitor")
+    @Query("select cr from  ChatRoom  cr join fetch cr.owner o join fetch cr.visitor where cr.id = :id")
     Optional<ChatRoom> findByIdWithOwnerAndVisitor(Long id);
+
 }
